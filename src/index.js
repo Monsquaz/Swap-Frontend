23cb6263e2a5fe3e50af2e9ddc70c9bc84f36a24
Patrik Weibull <patrik.weibull@gmail.com>
2018-11-28 18:43:11 +0000

    Removed unused params

--- src/index.js
+++ src/index.js
@@ -100,7 +100,7 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   routes,
   mode: 'history',
-  scrollBehavior (to, from, savedPosition) {
+  scrollBehavior () {
     return { x: 0, y: 0 }
   }
 })
